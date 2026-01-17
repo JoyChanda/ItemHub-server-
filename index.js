07f5daf7e00f391ddb4665c693ed2be192a9cbf8
JoyChanda <chandajoy1420@gmail.com>
2026-01-17 23:08:35 +0600

    fix(server): implement /api/items endpoint and fetch data on items page

--- index.js
+++ index.js
@@ -2,14 +2,19 @@ const express = require('express');
 const cors = require('cors');
 
 
+const items = require('./data/items');
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+// API Routes
+app.get('/api/items', (req, res) => {
+  res.json(items);
+});
 
 const PORT = 5000;
 
-
 app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
